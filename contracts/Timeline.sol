f07a1b1bde07b8a25d5202b0eede7ba9fa822d47
charles <charles.stays.relentless@gmail.com>
2021-12-20 23:09:49 -0500

    create a barebones timeline contract

---
+++ contracts/Timeline.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Timeline {
+    constructor() {
+        console.log("I spy with my eye a little birdy who is ready to tweet!");
+    }
+}
